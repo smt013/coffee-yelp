6b224e0b9157251c797c25484f1542964553342a
skylermt <smt013@ucsd.edu>
2019-11-10 23:20:26 -0800

    added @material-ui and progress on login page

--- webapp/src/views/LoginView/LoginView.js
+++ webapp/src/views/LoginView/LoginView.js
@@ -1,8 +1,26 @@
 import React from 'react'
+//import {TextField, Button, Checkbox} from '@material-ui/core'
+import {TextField, Button, Checkbox} from '@material-ui/core'
+import './LoginView.css'
 
 const LoginView = ({}) => {
 	return (
-		<h1> Hello </h1>
+		<div className="input" >
+			<div className="text-field">
+				<TextField classes="MuiFormControl-root MuiTextField-root" id="username" type="username" label="username" variant="outlined" />
+			</div>
+
+			<div className="text-field">
+				<TextField classes="MuiFormControl-root MuiTextField-root" id="password" type="password" label="password" variant="outlined" />
+			</div>
+
+			<div className="button">
+				<Button color="primary" variant="contained">
+					Login
+				</Button>
+			</div>
+
+		</div>
 	)
 }
 
