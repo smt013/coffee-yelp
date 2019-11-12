c07a28901921cba7d1c2e364780ef217a83bb40b
skylermt <smt013@ucsd.edu>
2019-11-11 17:17:04 -0800

    added components and styling to login page

--- webapp/src/views/LoginView/LoginView.js
+++ webapp/src/views/LoginView/LoginView.js
@@ -1,26 +1,53 @@
 import React from 'react'
 //import {TextField, Button, Checkbox} from '@material-ui/core'
-import {TextField, Button, Checkbox} from '@material-ui/core'
+import {TextField, Button, Checkbox, Container, FormControlLabel,CssBaseline,
+	Avatar, Grid, Typography} from '@material-ui/core'
+import LocalCafeIcon from '@material-ui/icons/LocalCafe';
 import './LoginView.css'
 
 const LoginView = ({}) => {
 	return (
-		<div className="input" >
-			<div className="text-field">
-				<TextField classes="MuiFormControl-root MuiTextField-root" id="username" type="username" label="username" variant="outlined" />
-			</div>
+		<Container component="main" maxWidth="xs">
+			<CssBaseline/>
+				<div id="input">
 
-			<div className="text-field">
-				<TextField classes="MuiFormControl-root MuiTextField-root" id="password" type="password" label="password" variant="outlined" />
-			</div>
+					<Grid container justify="center" alignItems="center">
+						<Avatar variant="circle" sizes="large">
+							<LocalCafeIcon fontSize="large"/>
+						</Avatar>
+					</Grid>
 
-			<div className="button">
-				<Button color="primary" variant="contained">
-					Login
-				</Button>
-			</div>
+						<div className="signin">
+							<Typography component="h1" variant="h5" align="center" className=".MuiTypography-root">
+								Sign in
+							</Typography>
+						</div>
 
-		</div>
+					<form>
+						<TextField classes="MuiFormControl-root MuiTextField-root"
+							id="username" type="Email Address"
+							label="Email Address *"
+							variant="outlined"
+							fullWidth="true"/>
+
+						<TextField classes="MuiFormControl-root MuiTextField-root"
+							id="password" type="Password" label="Password *"
+							variant="outlined"
+							fullWidth="true"/>
+
+							<FormControlLabel
+								control = {
+									<Checkbox value="remember" color="primary"/>
+								}
+								label="Remember me"
+							/>
+
+						<Button type="submit" color="primary" variant="contained" fullWidth="true">
+							Login
+						</Button>
+					</form>
+				</div>
+		</Container>
 	)
 }
 
